be94aa25531339e086904674c2255196ec5703ae
earmingol <earmingol14@gmail.com>
2019-06-07 02:40:35 -0700

    Initial commit

---
+++ cobra_utils/io/load_data.py
@@ -0,0 +1,39 @@
+import cobra
+
+
+def load_model(filename, format ='matlab'):
+    '''
+    This function opens a metabolic reconstruction from a given format.
+
+    Parameters
+    ----------
+    filename : str
+        Filename of the model to open. It is preferable to use absolute path.
+
+    format : str, 'matlab' by default.
+        Format of the file containing the model. Options to use:
+        'json' for .json file
+        'matlab' for .mat file
+        'sbml' for .xml file
+        'yaml' for  .yaml or .yml file
+
+    Returns
+    -------
+    model : cobra.core.Model.Model
+        The resulting cobra model.
+    '''
+    print('Loading genome-scale model')
+    try:
+        if format == 'json':
+            model = cobra.io.load_json_model(filename)
+        elif format == 'matlab':
+            model = cobra.io.load_matlab_model(filename)
+        elif format == 'sbml':
+            model = cobra.io.read_sbml_model(filename)
+        elif format == 'yaml':
+            model = cobra.io.load_yaml_model(filename)
+        else:
+            raise NotImplementedError("Format {} not implemented. Specify a correct format for the model".format(format))
+    except:
+        raise ImportError("The file has an incorrect format or does not match with implemented formats")
+    return model
---
+++ cobra_utils/query/rxn_info.py
@@ -0,0 +1,137 @@
+import pandas as pd
+
+
+def rxn_gene_associations_from_metabolites(model, metabolites, verbose=True):
+    '''
+    This function looks for all the reactions where the metabolites in the list participate. Also, it retrieves the genes
+    associated to those reactions.
+
+    Parameters
+    ----------
+    model : cobra.core.Model.Model
+        A cobra model.
+
+    metabolites : array-like
+        An iterable object containing a list of metabolite ids present in the model.
+
+    verbose : boolean, True by default.
+        A variable to enable or disable the printings of this function.
+
+    Returns
+    -------
+    rxn_gene_association : pandas.DataFrame
+        A pandas dataframe containing the information retrieved. The columns are :
+        'MetName', 'MetID', 'RxnID', 'RxnName', 'GeneID', 'Subsystem', 'RxnFormula'
+    '''
+    if verbose:
+        print('Using list of metabolites to get reactions where they participate. Also, getting genes of those reactions.')
+
+    rxn_gene_association = []
+    for metabolite in metabolites:
+        met = model.metabolites.get_by_id(metabolite)
+        for rxn in met.reactions:
+            for gene in rxn.genes:
+                rxn_gene_association.append(
+                    (met.name, met.id, rxn.id, rxn.name, gene.id, rxn.subsystem, rxn.reaction))
+
+    labels = ['MetName', 'MetID', 'RxnID', 'RxnName', 'GeneID', 'Subsystem', 'RxnFormula']
+    rxn_gene_association = pd.DataFrame.from_records(rxn_gene_association, columns=labels)
+    return rxn_gene_association
+
+
+def rxn_gene_associations_from_reactions(model, reactions, verbose=True):
+    '''
+    This function looks for all the reactions and genes that are associated from a list of reactions ids.
+
+    Parameters
+    ----------
+    model : cobra.core.Model.Model
+        A cobra model.
+
+    reactions : array-like
+        An iterable object containing a list of reaction ids present in the model.
+
+    verbose : boolean, True by default.
+        A variable to enable or disable the printings of this function.
+
+    Returns
+    -------
+    rxn_gene_association : pandas.DataFrame
+        A pandas dataframe containing the information retrieved. The columns are :
+        'GeneID', 'RxnID', 'RxnName', 'SubSystem', 'RxnFormula'
+    '''
+    if verbose:
+        print('Using list of reactions to get their information and genes associated.')
+
+    rxn_gene_association = []
+    for reaction in reactions:
+        rxn = model.reactions.get_by_id(reaction)
+        for gene in rxn.genes:
+            rxn_gene_association.append((str(gene.id), rxn.id, rxn.name, rxn.subsystem, rxn.reaction))
+    labels = ['GeneID', 'RxnID', 'RxnName', 'SubSystem', 'RxnFormula']
+    rxn_gene_association = pd.DataFrame.from_records(rxn_gene_association, columns=labels)
+    return rxn_gene_association
+
+
+def rxn_gene_associations_from_genes(model, genes, verbose=True):
+    '''
+    This function looks for all the reactions and genes that are associated from a list of gene ids.
+
+    Parameters
+    ----------
+    model : cobra.core.Model.Model
+        A cobra model.
+
+    genes : array-like
+        An iterable object containing a list of gene ids present in the model.
+
+    verbose : boolean, True by default.
+        A variable to enable or disable the printings of this function.
+
+    Returns
+    -------
+    rxn_gene_association : pandas.DataFrame
+        A pandas dataframe containing the information retrieved. The columns are :
+        'GeneID', 'RxnID', 'RxnName', 'SubSystem', 'RxnFormula'
+    '''
+    if verbose:
+        print('Using list of genes to get the reactions associated and their information.')
+
+    rxn_gene_association = []
+    for gene in genes:
+        g = model.genes.get_by_id(gene)
+        for rxn in g.reactions:
+                rxn_gene_association.append((str(g.id), rxn.id, rxn.name, rxn.subsystem, rxn.reaction))
+    labels = ['GeneID', 'RxnID', 'RxnName', 'SubSystem', 'RxnFormula']
+    rxn_gene_association = pd.DataFrame.from_records(rxn_gene_association, columns=labels)
+    return rxn_gene_association
+
+
+def rxn_gene_associations_from_model(model, verbose=True):
+    '''
+    This function looks for all the reactions in the model and returns their respective information.
+
+    Parameters
+    ----------
+    model : cobra.core.Model.Model
+        A cobra model.
+
+    verbose : boolean, True by default.
+        A variable to enable or disable the printings of this function.
+
+    Returns
+    -------
+    rxn_gene_association : pandas.DataFrame
+        A pandas dataframe containing the information retrieved. The columns are :
+        'GeneID', 'RxnID', 'RxnName', 'SubSystem', 'RxnFormula'
+    '''
+    if verbose:
+        print('Getting information for all reactions in the model.')
+
+    rxn_gene_association = []
+    for rxn in model.reactions:
+        for gene in rxn.genes:
+            rxn_gene_association.append((str(gene.id), rxn.id, rxn.name, rxn.subsystem, rxn.reaction))
+    labels = ['GeneID', 'RxnID', 'RxnName', 'SubSystem', 'RxnFormula']
+    rxn_gene_association = pd.DataFrame.from_records(rxn_gene_association, columns=labels)
+    return rxn_gene_association
