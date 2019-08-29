4810a9a2143dc38526319aa97006255b3b314cac
earmingol <earmingol14@gmail.com>
2019-08-29 15:50:47 -0700

    Fixed minor bugs

--- cobra_utils/query/__init__.py
+++ cobra_utils/query/__init__.py
@@ -2,6 +2,6 @@
 
 from __future__ import absolute_import
 
-from cobra_utils.query.get_ids import get_gene_list, get_met_list, get_rxn_list
+from cobra_utils.query.get_ids import get_gene_ids, get_met_ids, get_rxn_ids
 from cobra_utils.query.rxn_info import rxn_info_from_genes, rxn_info_from_metabolites, rxn_info_from_model, rxn_info_from_reactions
 from cobra_utils.query.met_info import met_info_from_genes, met_info_from_metabolites, met_info_from_model, met_info_from_reactions
